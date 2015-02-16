e00e9b4fb81e4116df1070ce7b033cfcc5e83ffa
joanvila <vila.joan94@gmail.com>
2015-02-16 23:20:30 +0100

    Home and colors first approach

--- js/main.js
+++ js/main.js
@@ -2,5 +2,34 @@ var cool = require('cool-ascii-faces');
 var bootstrap = require('bootstrap');
 
 $( document ).ready(function() {
-    $('#olakase').html(cool());
+    $('#coolFace').html(cool());
+});
+
+var professionSet = 0;
+
+window.setInterval(function(){
+	switch(professionSet) {
+	    case 0:
+	        $('#myProfession').fadeOut(1000, function() {
+  				$(this).html('Front-end Developer').fadeIn(1000);
+  			});
+	        ++professionSet;
+	        break;
+	    case 1:
+	    	$('#myProfession').fadeOut(1000, function() {
+  				$(this).html('Back-end Developer').fadeIn(1000);
+  			});
+	        ++professionSet;
+	        break;
+	    case 2:
+	    	$('#myProfession').fadeOut(1000, function() {
+  				$(this).html('Software Engineer').fadeIn(1000);
+  			});
+	        professionSet = 0;
+	        break;
+	}
+}, 4000);
+
+$( "#cooler" ).click(function() {
+  	$('#coolFace').html(cool());
 });
