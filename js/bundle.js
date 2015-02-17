46fc17ce36c8be489b6b9742b5030ffae7a47aca
joanvila <vila.joan94@gmail.com>
2015-02-17 23:18:28 +0100

    animate.css and wow.js added

--- js/bundle.js
+++ js/bundle.js
@@ -4,7 +4,7 @@ var cool = require('cool-ascii-faces');
 $( document ).ready(function() {
     $('#coolFace').html(cool());
     var homeHeight = window.innerHeight;
-    homeHeight = homeHeight - 55;
+    homeHeight = homeHeight;
     $('.home').css('height', homeHeight);
     var totalPad = homeHeight - $('.homeContainer').height();
     $('.homeContainer').css('padding-top', (totalPad/2));
@@ -13,66 +13,70 @@ $( document ).ready(function() {
     var professionSet = 0;
 
 	window.setInterval(function(){
+		$(this).removeClass('animated pulse');
 		switch(professionSet) {
 		    case 0:
 		        $('#myProfession').fadeOut(1000, function() {
 	  				$(this).html('Front-end Developer').fadeIn(1000);
+	  				$(this).addClass('animated pulse');
 	  			});
 		        ++professionSet;
 		        break;
 		    case 1:
 		    	$('#myProfession').fadeOut(1000, function() {
 	  				$(this).html('Back-end Developer').fadeIn(1000);
+	  				$(this).addClass('animated pulse');
 	  			});
 		        ++professionSet;
 		        break;
 		    case 2:
 		    	$('#myProfession').fadeOut(1000, function() {
 	  				$(this).html('PHP Developer').fadeIn(1000);
+	  				$(this).addClass('animated pulse');
 	  			});
 		        ++professionSet;
 		        break;
 		    case 3:
 		    	$('#myProfession').fadeOut(1000, function() {
 	  				$(this).html('JavaScript Lover').fadeIn(1000);
+	  				$(this).addClass('animated pulse');
 	  			});
 		        ++professionSet;
 		        break;
 		    case 4:
 		    	$('#myProfession').fadeOut(1000, function() {
 	  				$(this).html('Software Engineer').fadeIn(1000);
+	  				$(this).addClass('animated pulse');
 	  			});
 		        professionSet = 0;
 		        break;
 		}
 	}, 4000);
+
+	$( "#cooler" ).click(function() {
+  		$('#coolFace').html(cool());
+	});
+
+	$( "#homelnk" ).click(function() {
+	  	$('.home').animatescroll();
+	});
+
+	$( "#aboutlnk" ).click(function() {
+	  	$('.about').animatescroll();
+	});
+
+	$( "#projectslnk" ).click(function() {
+	  	$('.projects').animatescroll();
+	});
+
+	$( "#contactlnk" ).click(function() {
+	  	$('.contact').animatescroll();
+	});
+
+	$( "#wordslnk" ).click(function() {
+	  	$('.words').animatescroll();
+	});
 });
-
-$( "#cooler" ).click(function() {
-  	$('#coolFace').html(cool());
-});
-
-$( "#homelnk" ).click(function() {
-  	$('.home').animatescroll();
-});
-
-$( "#aboutlnk" ).click(function() {
-  	$('.about').animatescroll();
-});
-
-$( "#projectslnk" ).click(function() {
-  	$('.projects').animatescroll();
-});
-
-$( "#contactlnk" ).click(function() {
-  	$('.contact').animatescroll();
-});
-
-$( "#wordslnk" ).click(function() {
-  	$('.words').animatescroll();
-});
-
-
 },{"cool-ascii-faces":2}],2:[function(require,module,exports){
 var spigot = require("stream-spigot")
 
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var cool = require('cool-ascii-faces');
 $( document ).ready(function() {
     $('#coolFace').html(cool());
     var homeHeight = window.innerHeight;
-    homeHeight = homeHeight - 55;
+    homeHeight = homeHeight;
     $('.home').css('height', homeHeight);
     var totalPad = homeHeight - $('.homeContainer').height();
     $('.homeContainer').css('padding-top', (totalPad/2));
@@ -12,62 +12,67 @@ $( document ).ready(function() {
     var professionSet = 0;
 
 	window.setInterval(function(){
+		$(this).removeClass('animated pulse');
 		switch(professionSet) {
 		    case 0:
 		        $('#myProfession').fadeOut(1000, function() {
 	  				$(this).html('Front-end Developer').fadeIn(1000);
+	  				$(this).addClass('animated pulse');
 	  			});
 		        ++professionSet;
 		        break;
 		    case 1:
 		    	$('#myProfession').fadeOut(1000, function() {
 	  				$(this).html('Back-end Developer').fadeIn(1000);
+	  				$(this).addClass('animated pulse');
 	  			});
 		        ++professionSet;
 		        break;
 		    case 2:
 		    	$('#myProfession').fadeOut(1000, function() {
 	  				$(this).html('PHP Developer').fadeIn(1000);
+	  				$(this).addClass('animated pulse');
 	  			});
 		        ++professionSet;
 		        break;
 		    case 3:
 		    	$('#myProfession').fadeOut(1000, function() {
 	  				$(this).html('JavaScript Lover').fadeIn(1000);
+	  				$(this).addClass('animated pulse');
 	  			});
 		        ++professionSet;
 		        break;
 		    case 4:
 		    	$('#myProfession').fadeOut(1000, function() {
 	  				$(this).html('Software Engineer').fadeIn(1000);
+	  				$(this).addClass('animated pulse');
 	  			});
 		        professionSet = 0;
 		        break;
 		}
 	}, 4000);
-});
 
-$( "#cooler" ).click(function() {
-  	$('#coolFace').html(cool());
-});
+	$( "#cooler" ).click(function() {
+  		$('#coolFace').html(cool());
+	});
 
-$( "#homelnk" ).click(function() {
-  	$('.home').animatescroll();
-});
+	$( "#homelnk" ).click(function() {
+	  	$('.home').animatescroll();
+	});
 
-$( "#aboutlnk" ).click(function() {
-  	$('.about').animatescroll();
-});
+	$( "#aboutlnk" ).click(function() {
+	  	$('.about').animatescroll();
+	});
 
-$( "#projectslnk" ).click(function() {
-  	$('.projects').animatescroll();
-});
+	$( "#projectslnk" ).click(function() {
+	  	$('.projects').animatescroll();
+	});
 
-$( "#contactlnk" ).click(function() {
-  	$('.contact').animatescroll();
-});
+	$( "#contactlnk" ).click(function() {
+	  	$('.contact').animatescroll();
+	});
 
-$( "#wordslnk" ).click(function() {
-  	$('.words').animatescroll();
+	$( "#wordslnk" ).click(function() {
+	  	$('.words').animatescroll();
+	});
 });
-
